5a7d17cfc20668b6015ead978455b11531ba4258
JuanGoez18 <160356853+JuanGoez18@users.noreply.github.com>
2024-04-06 17:50:10 -0500

    appregistros
    prueva de parcial 1

---
+++ pruevaParcial/src/Cls_Candidatos.java
@@ -0,0 +1,89 @@
+
+public class Cls_Candidatos {
+    int idCl=0;
+    String nombreCl;
+    String apellidoCl;
+    String numerocontactoCl;
+    String correoCl;
+    String sexoCl;
+    String perfilProCl;
+    String estudiosCl;
+
+    public Cls_Candidatos(int idCl, String nombreCl, String apellidoCl, String numerocontactoCl, String correoCl,
+            String sexoCl, String perfilProCl, String estudiosCl) {
+        this.idCl = idCl;
+        this.nombreCl = nombreCl;
+        this.apellidoCl = apellidoCl;
+        this.numerocontactoCl = numerocontactoCl;
+        this.correoCl = correoCl;
+        this.sexoCl = sexoCl;
+        this.perfilProCl = perfilProCl;
+        this.estudiosCl = estudiosCl;
+    }
+
+    public int getIdCl() {
+        return idCl;
+    }
+
+    public void setIdCl(int idCl) {
+        this.idCl = idCl;
+    }
+
+    public String getNombreCl() {
+        return nombreCl;
+    }
+
+    public void setNombreCl(String nombreCl) {
+        this.nombreCl = nombreCl;
+    }
+
+    public String getApellidoCl() {
+        return apellidoCl;
+    }
+
+    public void setApellidoCl(String apellidoCl) {
+        this.apellidoCl = apellidoCl;
+    }
+
+    public String getNumerocontactoCl() {
+        return numerocontactoCl;
+    }
+
+    public void setNumerocontactoCl(String numerocontactoCl) {
+        this.numerocontactoCl = numerocontactoCl;
+    }
+
+    public String getCorreoCl() {
+        return correoCl;
+    }
+
+    public void setCorreoCl(String correoCl) {
+        this.correoCl = correoCl;
+    }
+
+    public String getSexoCl() {
+        return sexoCl;
+    }
+
+    public void setSexoCl(String sexoCl) {
+        this.sexoCl = sexoCl;
+    }
+
+    public String getPerfilProCl() {
+        return perfilProCl;
+    }
+
+    public void setPerfilProCl(String perfilProCl) {
+        this.perfilProCl = perfilProCl;
+    }
+
+    public String getEstudiosCl() {
+        return estudiosCl;
+    }
+
+    public void setEstudiosCl(String estudiosCl) {
+        this.estudiosCl = estudiosCl;
+    }
+
+    
+}
